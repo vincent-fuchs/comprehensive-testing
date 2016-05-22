6d5e7c7547ed5d1d287686b141823607f95f49bf
Anand Manissery <anand.manissery@gmail.com>
2016-05-22 16:09:03 +0530

    Removed unused imports

--- registry-server/src/main/java/org/example/registry/RegistryApp.java
+++ registry-server/src/main/java/org/example/registry/RegistryApp.java
@@ -2,8 +2,6 @@ package org.example.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
