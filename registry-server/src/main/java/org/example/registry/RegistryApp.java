690f1a8422efe60adb014474dbc3af197e6ec9f8
anandmnair <anand.manissery@gmail.com>
2016-05-22 16:06:14 +0530

    Updated Registry Server

--- registry-server/src/main/java/org/example/registry/RegistryApp.java
+++ registry-server/src/main/java/org/example/registry/RegistryApp.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Created by Anand
+ *
+ * @author <a href="mailto:anand.manissery@gmail.com">Anand Manissery</a>
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistryApp {
