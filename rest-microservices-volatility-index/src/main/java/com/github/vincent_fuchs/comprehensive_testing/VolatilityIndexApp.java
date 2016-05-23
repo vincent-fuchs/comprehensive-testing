ff6113ba319862f9fa13eb54db9b37cb9e7333e6
Vincent FUCHS <vincent.fuchs@gmail.com>
2016-05-23 22:33:50 +0530

    moved classes in right package

---
+++ rest-microservices-volatility-index/src/main/java/com/github/vincent_fuchs/comprehensive_testing/VolatilityIndexApp.java
@@ -0,0 +1,19 @@
+package com.github.vincent_fuchs.comprehensive_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by Paul
+ *
+ * @author <a href="mailto:paul58914080@gmail.com">Paul Williams</a>
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class VolatilityIndexApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolatilityIndexApp.class, args);
+    }
+}
